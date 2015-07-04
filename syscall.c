26b5479090976cb6219b90ffdee7bae06d875e65
System Administrator <root@Akashs-MacBook-Pro.local>
2015-07-04 23:24:29 +0530

    Added comments to code

--- syscall.c
+++ syscall.c
@@ -3,18 +3,22 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-char *file_name = "test.txt";
+// set up file Name and stat struct
 struct stat *buff;
 
-void get_stat(){
+void get_stat(char *file_name){
+  // Allocate memory to stat *buff
   buff = malloc(sizeof(struct stat));
   stat(file_name,buff);
+  // Get size of file in int
   int size = buff->st_size;
-  printf("size of the file is %d",size);
+  printf("size of the file is %d ",size);
+  // Release the memory 
   free(buff);
 }
 
 int main(){
- get_stat();
+  // get stat
+  get_stat("test.txt");
   return 0;
 }
